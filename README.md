da73bde5b1241ef9463ac4d6c96be5306ea2e202
Paul Edemekong <hello.paulvin@gmail.com>
2022-07-13 14:20:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## TodoMe
+
+A project to learn about CRUD operations using TODO app as a case study.
---
+++ android/app/src/main/kotlin/com/example/todome/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todome
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
